5988d9e69b59419d07e3ca61466519aea03e18e6
GMenegatto <gabrielmenegattogf@hotmail.com>
2020-06-10 15:35:53 -0300

    app

---
+++ mobile/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo'
+import Routes from './src/routes'
+
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+
+import Home from './src/pages/Home';
+
+export default function App() {
+
+  const [fontLoaded] = useFonts(
+    {
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Ubuntu_700Bold
+    }
+);
+
+if(!fontLoaded){
+    return <AppLoading/>
+}
+
+  return (
+    <>
+    <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+    <Routes />
+    </>
+  );
+}
+
---
+++ web/src/routes.tsx
@@ -0,0 +1,16 @@
+import { Route, BrowserRouter, /*RouteProps*/ } from 'react-router-dom';
+import React from 'react';
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Route component={Home} path="/" exact/>
+            <Route component={CreatePoint} path="/create-point"/>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
